759f396f5085c7ed64f5ada28c2a55c42ba01096
Bryan Chan <103339794+bchan9803@users.noreply.github.com>
2024-01-25 15:42:51 -0800

    Implemented form and integrated it with the Nodemailer API

--- src/components/NewsForm.tsx
+++ src/components/NewsForm.tsx
@@ -1,14 +1,107 @@
-const handleSubmit = () => {
-  alert('hi')
-}
+import { useState } from 'react'
 
-const NewsForm = ({ children }) => {
+const NewsForm = () => {  
+  const [newsOutlet, setNewsOutlet] = useState('')
+  const [email, setEmail] = useState('')
+
+  const NewsOption = (props) => {
+    return (
+      <div className="form-check">
+        <input 
+          type="radio" 
+          className="form-check-input" 
+          name='news-outlet'
+          id={props.newsOptionID}
+          value={props.newsOptionVal}
+          checked={props.isChecked} 
+          onChange={(e) => setNewsOutlet(e.target.value)}
+        />
+        <label htmlFor={props.newsOptionID} className="form-check-label">{props.newsOptionLabel}</label>
+      </div>
+    )
+  }
+  
+  const handleSubmit =  (e: Event) => {
+    // sends email details to nodemailer api in '/src/services/nodemailer api/ app.js'
+  
+    // Plan
+    // 1. get form to submit by sending POST request to nodemailer api
+    // 2. then, add useState to append val to emailRecipient, emailSubject, emailText
+
+
+    console.log('handle submit triggered');  
+    e.preventDefault()            // prevents page reloading
+  
+    const testRecipient = email
+    const testSubject = newsOutlet
+    const text = 'Eiusmod laboris incididunt id incididunt consectetur ea veniam anim irure sint laboris quis.'
+  
+    fetch("http://localhost:3000/api/email", {
+      method: "POST",
+      body: JSON.stringify({
+        emailRecipient: testRecipient,
+        emailSubject: testSubject,
+        emailText: text,
+      }),
+      headers: {
+        "Content-type": "application/json; charset=UTF-8"
+      }
+    })
+    .then((response) => response.json())
+    .then((json) => console.log(json));
+  
+    // document.querySelector('.alert').style.display = 'block'
+  }
+
   return (
     <form 
-      onSubmit={handleSubmit}
+      onSubmit ={handleSubmit}
       className=' border border-light-subtle my-0 mx-auto p-4 w-50'
     >
-      {children}
+      <h3 className='fs-5'>Choose a news outlet to recieve updates from: </h3>
+      {/* news outlet input field */}
+      {/* Axios */}
+      <NewsOption 
+        newsOptionID='axios'
+        newsOptionLabel='Axios'
+        newsOptionVal='axios'
+        isChecked={newsOutlet === 'axios'}
+      />
+      {/* BBC News */}
+      <NewsOption 
+        newsOptionID='bbc'
+        newsOptionLabel='BBC News'
+        newsOptionVal='bbc'
+        isChecked={newsOutlet === 'bbc'}
+      />
+      {/* New York Times */}
+      <NewsOption 
+        newsOptionID='nyt'
+        newsOptionLabel='New York Times'
+        newsOptionVal='nyt'
+        isChecked={newsOutlet === 'nyt'}
+      />
+
+      <br />
+
+      <h3 className='fs-5'>Enter your email address:</h3>
+      {/* email input field */}
+      <section className='mb-5'>
+        <label htmlFor="email-input-field">Email address</label>
+        <input 
+          type="email" 
+          className='form-control' 
+          id='email-input-field' 
+          placeholder='example@mail.com'
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+      </section>
+
+      {/* submit btn */}
+      <button type='submit' className='btn btn-primary'>
+        Submit
+      </button>
     </form>
   )
 }
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,54 +1,8 @@
 import NewsForm from "../components/NewsForm"
-import NewsOutletInputField from "../components/NewsOutletInputField"
-import EmailInputField from "../components/EmailInputField"
-// import SubmitBtn from "../components/SubmitBtn"
-
-// Plan
-// 1. get form to submit by sending POST request to nodemailer api
-// 2. then, add useState to append val to emailRecipient, emailSubject, emailText
-
-
-const openAlert = () => {
-  // alert()
-
-  // sends email details to nodemailer api in '/src/services/nodemailer api/ app.js'
-  fetch("http://localhost:3000/api/email", {
-    method: "POST",
-    body: JSON.stringify({
-      emailRecipient: 'bchan9803@gmail.com',
-      emailSubject: 'mike',
-      emailText: 'pence',
-    }),
-    headers: {
-      "Content-type": "application/json; charset=UTF-8"
-    }
-  })
-  .then((response) => response.json())
-  .then((json) => console.log(json));
-
-
-
-  document.querySelector('.alert').style.display = 'block'
-}
-
 const Home = () => {
   return (
     <main>
-      <NewsForm>
-        <h3 className='fs-4'>Choose a news outlet to recieve updates from: </h3>
-        <NewsOutletInputField />
-        <EmailInputField />
-        {/* <SubmitBtn /> */}
-
-      <button 
-        type='button' 
-        className='btn btn-primary' 
-        onClick={openAlert}
-      >
-        Submit
-      </button>
-
-      </NewsForm>
+      <NewsForm />
     </main>
   )
 }

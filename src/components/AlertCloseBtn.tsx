641df4c29f959ac965c2a14c2412fcca8d8f1026
Bryan Chan <103339794+bchan9803@users.noreply.github.com>
2024-01-29 14:07:18 -0800

    Fixed some bugs

--- src/components/AlertCloseBtn.tsx
+++ src/components/AlertCloseBtn.tsx
@@ -2,10 +2,10 @@
 const AlertCloseBtn = () => {
   // script to close the alert modal
   const closeAlert = () => {
-    document.querySelector('.alert').style.display = 'none'
+    // document.querySelector('.alert').style.display = 'none'
   }
   return (
-    <button type='button' class='btn fs-5' onClick={closeAlert}>x</button>
+    <button type='button' className='btn fs-5' onClick={closeAlert}>x</button>
   )
 }
 

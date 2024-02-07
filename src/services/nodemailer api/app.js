035df8d93a88803f931eae35f03007c0bd2ef54b
Bryan Chan <103339794+bchan9803@users.noreply.github.com>
2024-02-07 14:55:28 -0800

    ...

--- src/services/nodemailer api/app.js
+++ src/services/nodemailer api/app.js
@@ -1,11 +1,13 @@
 import express from 'express';
 import mongoose from 'mongoose';
+import cors from 'cors';
 import { UserRouter } from './routes/users.js';
 import { APIRouter } from './routes/api.js';
 
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 
 mongoose.connect("mongodb+srv://449studios:4949PostPostMan@users.abbiwhk.mongodb.net/postDB");
 // mongoose.connect(`${process.env.REACT_MONGODB_CONNECTION_STRING}`);
